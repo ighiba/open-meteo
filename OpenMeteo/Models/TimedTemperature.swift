8006494b16e94006c95527e7edb6b744b600c482
ighiba <ighiba18@gmail.com>
2023-06-25 00:02:26 +0300

    Added Weather object

---
+++ OpenMeteo/Models/TimedTemperature.swift
@@ -0,0 +1,13 @@
+//
+//  TimedTemperature.swift
+//  OpenMeteo
+//
+//  Created by Ivan Ghiba on 24.06.2023.
+//
+
+import Foundation
+
+struct TimedTemperature {
+    var time: Date
+    var temperature: Float
+}
---
+++ OpenMeteo/Models/Weather.swift
@@ -0,0 +1,12 @@
+//
+//  Weather.swift
+//  OpenMeteo
+//
+//  Created by Ivan Ghiba on 24.06.2023.
+//
+
+import Foundation
+
+struct Weather {
+    var timedTemperatureList: [TimedTemperature]
+}
