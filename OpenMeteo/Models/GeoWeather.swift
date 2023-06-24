bc2d060dcc8b63d58ae763cc664364a8eaa6f6be
ighiba <ighiba18@gmail.com>
2023-06-25 00:04:46 +0300

    Added GeoWeather object

---
+++ OpenMeteo/Models/GeoWeather.swift
@@ -0,0 +1,13 @@
+//
+//  GeoWeather.swift
+//  OpenMeteo
+//
+//  Created by Ivan Ghiba on 24.06.2023.
+//
+
+import Foundation
+
+struct GeoWeather {
+    var geocoding: Geocoding
+    var weather: Weather
+}
