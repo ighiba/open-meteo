b7e529f0ecff1a7ecbe50ed9e111ec3a87c1d87d
ighiba <ighiba18@gmail.com>
2023-06-26 13:53:40 +0300

    Added TemperatureLabel

---
+++ OpenMeteo/Views/TemperatureLabel.swift
@@ -0,0 +1,29 @@
+//
+//  TemperatureLabel.swift
+//  OpenMeteo
+//
+//  Created by Ivan Ghiba on 26.06.2023.
+//
+
+import UIKit
+
+class TemperatureLabel: UILabel {
+    
+    init(_ temperature: Float? = nil) {
+        super.init(frame: .zero)
+        if let temperature = temperature {
+            setTemperature(temperature)
+        } else {
+            self.text = "--"
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setTemperature(_ temperature: Float) {
+        self.text = "\(Int(temperature))°C"
+        self.sizeToFit()
+    }
+}
