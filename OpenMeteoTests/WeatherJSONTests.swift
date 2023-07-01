5780d70e743ec02ac642941e8c90420b4d4a988e
ighiba <ighiba18@gmail.com>
2023-07-01 15:11:18 +0300

    Updated tests, added OpenMeteo.testplan

--- OpenMeteoTests/WeatherJSONTests.swift
+++ OpenMeteoTests/WeatherJSONTests.swift
@@ -18,34 +18,15 @@ final class WeatherJSONTests: XCTestCase {
         let correctTime = dateFormatter.date(from: "2023-06-25T17:00:00+00:00")
         
         let weather = try decoder.decode(WeatherJSON.self, from: weatherJSONData).weather
-        let time = weather.current.time
+        let time = weather.obtainForecastForCurrentHour().date
 
         XCTAssertEqual(time, correctTime, "Wrong value decode result")
     }
     
     func testDecodeCurrentTemperature() throws {
         let weather = try decoder.decode(WeatherJSON.self, from: weatherJSONData).weather
-        let temperature = weather.current.temperature
+        let temperature = weather.obtainForecastForCurrentHour().temperature
 
         XCTAssertEqual(temperature, 21.0, "Wrong value decode result")
     }
-    
-    func testDecodeForecastTimeLast() throws {
-        let dateFormatter = ISO8601DateFormatter()
-        dateFormatter.formatOptions = .withInternetDateTime
-        let correctTime = dateFormatter.date(from: "2023-07-01T23:00:00+00:00")
-        
-        let weather = try decoder.decode(WeatherJSON.self, from: weatherJSONData).weather
-        let time = weather.forecastByHour.last?.time
-
-        XCTAssertEqual(time, correctTime, "Wrong value decode result")
-    }
-    
-    func testDecodeForecastTemperatureLast() throws {
-        let weather = try decoder.decode(WeatherJSON.self, from: weatherJSONData).weather
-        let temperature = weather.forecastByHour.last?.temperature
-
-        XCTAssertEqual(temperature, 15.2, "Wrong value decode result")
-    }
-    
 }
