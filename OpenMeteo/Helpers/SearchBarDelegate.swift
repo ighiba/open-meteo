e64afddee69d7e1eb9480b3c105c791ba902bdc3
ighiba <ighiba18@gmail.com>
2023-07-07 14:58:50 +0300

    Moved SearchBarDelete to own file

---
+++ OpenMeteo/Helpers/SearchBarDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SearchBarDelegate.swift
+//  OpenMeteo
+//
+//  Created by Ivan Ghiba on 07.07.2023.
+//
+
+import UIKit
+
+class SearchBarDelegate: NSObject, UISearchBarDelegate {
+    
+    var textDidChangeHandler: ((String) -> Void)?
+    var searchDidClickHandler: ((String) -> Void)?
+    
+    init(textDidChangeHandler: ((String) -> Void)?, searchDidClickHandler: ((String) -> Void)?) {
+        self.textDidChangeHandler = textDidChangeHandler
+        self.searchDidClickHandler = searchDidClickHandler
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text, !searchText.isEmpty else { return }
+        searchDidClickHandler?(searchText)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        textDidChangeHandler?(searchText)
+    }
+}
+
--- OpenMeteo/Modules/GeoSearch/GeoSearchViewController.swift
+++ OpenMeteo/Modules/GeoSearch/GeoSearchViewController.swift
@@ -125,23 +125,3 @@ extension GeoSearchViewController: UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
-
-class SearchBarDelegate: NSObject, UISearchBarDelegate {
-    
-    var textDidChangeHandler: ((String) -> Void)?
-    var searchDidClickHandler: ((String) -> Void)?
-    
-    init(textDidChangeHandler: ((String) -> Void)?, searchDidClickHandler: ((String) -> Void)?) {
-        self.textDidChangeHandler = textDidChangeHandler
-        self.searchDidClickHandler = searchDidClickHandler
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let searchText = searchBar.text, !searchText.isEmpty else { return }
-        searchDidClickHandler?(searchText)
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        textDidChangeHandler?(searchText)
-    }
-}
