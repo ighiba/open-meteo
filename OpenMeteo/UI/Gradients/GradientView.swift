f3efbb6afa01903939a8912bcb21205ecf7feabc
ighiba <ighiba18@gmail.com>
2024-02-11 21:51:05 +0300

    Refactored GradientView

--- OpenMeteo/UI/Gradients/GradientView.swift
+++ OpenMeteo/UI/Gradients/GradientView.swift
@@ -7,36 +7,31 @@
 
 import UIKit
 
-class GradientView: UIView {
+final class GradientView: UIView {
     
     private let gradientLayer = CAGradientLayer()
     
-    var startColor: UIColor = .white {
-        didSet {
-            updateGradientColors()
-        }
-    }
-    var endColor: UIColor = .white {
-        didSet {
-            updateGradientColors()
-        }
-    }
+    private(set) var startColor: UIColor = .white
+    private(set) var endColor: UIColor = .white
     
     override class var layerClass: AnyClass { CAGradientLayer.self }
     
+    // MARK: - Init
+    
     init(startPoint: CGPoint = CGPoint(x: 0.5, y: 0.0), endPoint: CGPoint = CGPoint(x: 0.5, y: 1.0)) {
         super.init(frame: .zero)
-        layer.addSublayer(gradientLayer)
-        gradientLayer.startPoint = startPoint
-        gradientLayer.endPoint = endPoint
+        self.setupGradient(startPoint: startPoint, endPoint: endPoint)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Methods
+    
     override func layoutSubviews() {
         super.layoutSubviews()
+        
         gradientLayer.frame = bounds
     }
     
@@ -44,16 +39,21 @@ class GradientView: UIView {
         gradientLayer.frame = bounds
     }
     
-    func setColors(start startColor: UIColor? = nil, end endColor: UIColor? = nil) {
-        if let startColor {
-            self.startColor = startColor
-        }
-        if let endColor {
-            self.endColor = endColor
-        }
+    private func setupGradient(startPoint: CGPoint, endPoint: CGPoint) {
+        layer.addSublayer(gradientLayer)
+        
+        gradientLayer.startPoint = startPoint
+        gradientLayer.endPoint = endPoint
     }
     
-    func updateGradientColors() {
+    func setColors(start newStartColor: UIColor, end newEndColor: UIColor) {
+        startColor = newStartColor
+        endColor = newEndColor
+        
+        updateGradientColors()
+    }
+    
+    private func updateGradientColors() {
         gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
     }
 }
